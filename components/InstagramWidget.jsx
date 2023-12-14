07485a3214757e9f9f1a8213e8366481235263d0
Luis Landaeta <luis.landaeta24@gmail.com>
2023-12-14 17:49:33 -0400

    Fix instagram widget to proper desktop sizing

--- components/InstagramWidget.jsx
+++ components/InstagramWidget.jsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 /* eslint-disable tailwindcss/no-custom-classname */
 const InstagramWidget = () => {
   return (
-    <article className="container mx-auto w-[80vw] py-[5vh]">
+    <article className="container mx-auto w-[80vw] py-[5vh] lg:w-[60vw]">
       <Suspense>
         <div
           className="elfsight-app-d7adc22f-21c7-4d9c-8e44-5f2b64256b27"
