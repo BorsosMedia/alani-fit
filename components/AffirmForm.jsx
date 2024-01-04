fada60d221d2f73914a2ea9ee6f1d88bb82117be
Luis Landaeta <luis.landaeta24@gmail.com>
2024-01-04 14:16:18 -0400

    Comment unfinished Affirm Form work

--- components/AffirmForm.jsx
+++ components/AffirmForm.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
@@ -90,151 +90,151 @@ const formSchema = z.object({
 });
 
 const AffirmForm = ({ onClose, TypeOfPlan }) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  function handleAffirmButton(data) {
-    affirm.checkout({
-      merchant: {
-        user_confirmation_url: `${baseUrl}/checkout`,
-        user_cancel_url: `${baseUrl}`,
-        user_confirmation_url_action: "GET",
-        name: "Bodybuilding & BS",
-      },
-      shipping: {
-        name: {
-          full: data.shipping_name,
-        },
-        address: {
-          line1: data.shipping_address_line1,
-          line2: data.shipping_address_line2,
-          city: data.shipping_address_city,
-          state: data.shipping_address_state,
-          zipcode: data.shipping_address_zipcode,
-          country: data.shipping_address_country,
-        },
-        phone_number: data.shipping_phone_number,
-        email: data.shipping_email,
-      },
-      billing: {
-        name: {
-          full: data.billing_name,
-        },
-        address: {
-          line1: data.billing_address_line1,
-          line2: data.billing_address_line2,
-          city: data.billing_address_city,
-          state: data.billing_address_state,
-          zipcode: data.billing_address_zipcode,
-          country: data.billing_address_country,
-        },
-        phone_number: data.billing_phone_number,
-        email: data.billing_email,
-      },
-      items: [
-        {
-          display_name: (() => {
-            switch (TypeOfPlan) {
-              case "a":
-                return "Monthly";
-              case "b":
-                return "3-month";
-              case "d":
-                return "6-Month";
-              case "e":
-                return "Annual";
-              case "f":
-                return "Bloodwork";
-              case "g":
-                return "Gut Health Test";
-            }
-          })(),
-          sku: (() => {
-            switch (TypeOfPlan) {
-              case "a":
-                return "Monthly";
-              case "b":
-                return "3-month";
-              case "d":
-                return "6-Month";
-              case "e":
-                return "Annual";
-              case "f":
-                return "Bloodwork";
-              case "g":
-                return "Gut Health Test";
-            }
-          })(),
-          unit_price: (() => {
-            switch (TypeOfPlan) {
-              case "a":
-                return 29500;
-              case "b":
-                return 79650;
-              case "d":
-                return 150450;
-              case "e":
-                return 283200;
-              case "f":
-                return 29900;
-              case "g":
-                return 79900;
-            }
-          })(),
-          qty: 1,
-        },
-      ],
-      metadata: {
-        mode: "modal",
-      },
-      order_id: crypto.randomUUID(),
-      currency: "USD",
-      shipping_amount: 0,
-      tax_amount: 0,
-      total: (() => {
-        switch (TypeOfPlan) {
-          case "a":
-            return 29500;
-          case "b":
-            return 79650;
-          case "d":
-            return 150450;
-          case "e":
-            return 283200;
-          case "f":
-            return 29900;
-          case "g":
-            return 79900;
-        }
-      })(),
-    });
+  // const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  // function handleAffirmButton(data) {
+  //   affirm.checkout({
+  //     merchant: {
+  //       user_confirmation_url: `${baseUrl}/checkout`,
+  //       user_cancel_url: `${baseUrl}`,
+  //       user_confirmation_url_action: "GET",
+  //       name: "Bodybuilding & BS",
+  //     },
+  //     shipping: {
+  //       name: {
+  //         full: data.shipping_name,
+  //       },
+  //       address: {
+  //         line1: data.shipping_address_line1,
+  //         line2: data.shipping_address_line2,
+  //         city: data.shipping_address_city,
+  //         state: data.shipping_address_state,
+  //         zipcode: data.shipping_address_zipcode,
+  //         country: data.shipping_address_country,
+  //       },
+  //       phone_number: data.shipping_phone_number,
+  //       email: data.shipping_email,
+  //     },
+  //     billing: {
+  //       name: {
+  //         full: data.billing_name,
+  //       },
+  //       address: {
+  //         line1: data.billing_address_line1,
+  //         line2: data.billing_address_line2,
+  //         city: data.billing_address_city,
+  //         state: data.billing_address_state,
+  //         zipcode: data.billing_address_zipcode,
+  //         country: data.billing_address_country,
+  //       },
+  //       phone_number: data.billing_phone_number,
+  //       email: data.billing_email,
+  //     },
+  //     items: [
+  //       {
+  //         display_name: (() => {
+  //           switch (TypeOfPlan) {
+  //             case "a":
+  //               return "Monthly";
+  //             case "b":
+  //               return "3-month";
+  //             case "d":
+  //               return "6-Month";
+  //             case "e":
+  //               return "Annual";
+  //             case "f":
+  //               return "Bloodwork";
+  //             case "g":
+  //               return "Gut Health Test";
+  //           }
+  //         })(),
+  //         sku: (() => {
+  //           switch (TypeOfPlan) {
+  //             case "a":
+  //               return "Monthly";
+  //             case "b":
+  //               return "3-month";
+  //             case "d":
+  //               return "6-Month";
+  //             case "e":
+  //               return "Annual";
+  //             case "f":
+  //               return "Bloodwork";
+  //             case "g":
+  //               return "Gut Health Test";
+  //           }
+  //         })(),
+  //         unit_price: (() => {
+  //           switch (TypeOfPlan) {
+  //             case "a":
+  //               return 29500;
+  //             case "b":
+  //               return 79650;
+  //             case "d":
+  //               return 150450;
+  //             case "e":
+  //               return 283200;
+  //             case "f":
+  //               return 29900;
+  //             case "g":
+  //               return 79900;
+  //           }
+  //         })(),
+  //         qty: 1,
+  //       },
+  //     ],
+  //     metadata: {
+  //       mode: "modal",
+  //     },
+  //     order_id: crypto.randomUUID(),
+  //     currency: "USD",
+  //     shipping_amount: 0,
+  //     tax_amount: 0,
+  //     total: (() => {
+  //       switch (TypeOfPlan) {
+  //         case "a":
+  //           return 29500;
+  //         case "b":
+  //           return 79650;
+  //         case "d":
+  //           return 150450;
+  //         case "e":
+  //           return 283200;
+  //         case "f":
+  //           return 29900;
+  //         case "g":
+  //           return 79900;
+  //       }
+  //     })(),
+  //   });
 
-    affirm.checkout.open();
-  }
+  //   affirm.checkout.open();
+  // }
 
-  const [info, setInfo] = useState({
-    shipping_name: "",
+  // const [info, setInfo] = useState({
+  //   shipping_name: "",
 
-    shipping_address_line1: "",
-    shipping_address_line2: "",
-    shipping_address_city: "",
-    shipping_address_state: "",
-    shipping_address_zipcode: "",
-    shipping_address_country: "",
+  //   shipping_address_line1: "",
+  //   shipping_address_line2: "",
+  //   shipping_address_city: "",
+  //   shipping_address_state: "",
+  //   shipping_address_zipcode: "",
+  //   shipping_address_country: "",
 
-    shipping_phone_number: "",
-    shipping_email: "",
+  //   shipping_phone_number: "",
+  //   shipping_email: "",
 
-    billing_name: "",
+  //   billing_name: "",
 
-    billing_address_line1: "",
-    billing_address_line2: "",
-    billing_address_city: "",
-    billing_address_state: "",
-    billing_address_zipcode: "",
-    billing_address_country: "",
+  //   billing_address_line1: "",
+  //   billing_address_line2: "",
+  //   billing_address_city: "",
+  //   billing_address_state: "",
+  //   billing_address_zipcode: "",
+  //   billing_address_country: "",
 
-    billing_phone_number: "",
-    billing_email: "",
-  });
+  //   billing_phone_number: "",
+  //   billing_email: "",
+  // });
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -263,21 +263,21 @@ const AffirmForm = ({ onClose, TypeOfPlan }) => {
 
   const onSubmit = () => {};
 
-  const handleChange = (e) => {
-    setInfo({
-      ...info,
-      [e.target.name]: e.target.value,
-    });
-    console.log(data);
-  };
+  // const handleChange = (e) => {
+  //   setInfo({
+  //     ...info,
+  //     [e.target.name]: e.target.value,
+  //   });
+  //   console.log(data);
+  // };
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(formSchema),
-  });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm({
+  //   resolver: zodResolver(formSchema),
+  // });
 
   return (
     <Form {...form}>
